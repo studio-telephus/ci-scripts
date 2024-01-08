e9c45af134c28fee43ccc979e2f396d3c00cc37e
Kristo Aun <kristoaun@live.com>
2024-01-08 15:24:18 +0200

    keep

--- run-version/semver.sh
+++ run-version/semver.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-MAJOR_REVISION=$(echo $(date +"%y%m") | sed 's/^0*//')
-MINOR_REVISION=$(echo $(date +"%d%H") | sed 's/^0*//')
-BUILD_REVISION=$(echo $(date +"%M%S") | sed 's/^0*//')
-BUILD_REVISION_MILLI=$(echo $(date +"%3N") | sed 's/^0*//' | cut -c1-1)
+if [ -z "${SEMVER}" ]; then
+  MAJOR_REVISION=$(echo $(date +"%y%m") | sed 's/^0*//')
+  MINOR_REVISION=$(echo $(date +"%d%H") | sed 's/^0*//')
+  BUILD_REVISION=$(echo $(date +"%M%S") | sed 's/^0*//')
+  BUILD_REVISION_MILLI=$(echo $(date +"%3N") | sed 's/^0*//' | cut -c1-1)
 
-export SEMVER=$MAJOR_REVISION.$MINOR_REVISION.$BUILD_REVISION$BUILD_REVISION_MILLI
-echo "Semver value ${SEMVER} has been exported to the environment variable SEMVER."
+  export SEMVER=$MAJOR_REVISION.$MINOR_REVISION.$BUILD_REVISION$BUILD_REVISION_MILLI
+  echo "Semver value ${SEMVER} has been exported to the environment variable SEMVER."
+else
+  echo "Found semver ${SEMVER} from environment. Keeping the existing value."
+fi
+
+
