86e931b25b96c44cb4ae2595ca13e20e93b0112f
Kristo Aun <kristoaun@live.com>
2024-01-08 14:52:59 +0200

    multitag

--- run-docker-project/publish.sh
+++ run-docker-project/publish.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-: "${CI_PUBLISH_VERSION?}"
+: "${CI_PUBLISH_TAGS?}"
 : "${CI_NEXUS_DOCKER_PUBLISH_PASSWORD?}"
 : "${CI_NEXUS_DOCKER_PUBLISH_USER?}"
 : "${CI_NEXUS_DOCKER_URL?}"
 
 DOCKER_REPOSITORY_URI="$CI_NEXUS_DOCKER_URL/$CI_REGISTRY_IMAGE"
+
+tags=(${IN//,/ })
+first_tag=${tags[0]}
+tags_length=${#tags[@]}
 
 echo -n $CI_NEXUS_DOCKER_PUBLISH_PASSWORD | docker login -u $CI_NEXUS_DOCKER_PUBLISH_USER --password-stdin $CI_NEXUS_DOCKER_URL
 
@@ -19,7 +23,20 @@ docker build \
   --pull \
   --build-arg VCS_URL=$CI_PROJECT_URL \
   --cache-from $DOCKER_REPOSITORY_URI:latest \
-  --tag $DOCKER_REPOSITORY_URI:$CI_PUBLISH_VERSION \
+  --tag $DOCKER_REPOSITORY_URI:$first_tag \
   .
 
-docker push $DOCKER_REPOSITORY_URI:$CI_PUBLISH_VERSION
+docker push $DOCKER_REPOSITORY_URI:$first_tag
+
+if [ $tags_length -gt 1 ]; then
+  echo "Found additional tags. Proceeding to tag & Push."
+  for ((i = 1; i < ${tags_length}; i++)); do
+    tg=${tags[$i]}
+    docker tag $CI_REGISTRY_IMAGE:$first_tag $CI_REGISTRY_IMAGE:$tg
+    docker push $DOCKER_REPOSITORY_URI:$tg
+  done
+else
+  echo "No additional tags found."
+fi
+
+
