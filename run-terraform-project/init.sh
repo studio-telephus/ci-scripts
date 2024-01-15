854c998d4f81486f20979dd443c3231f1af74e15
Kristo Aun <kristoaun@live.com>
2024-01-15 10:11:22 +0200

    new vars

--- run-terraform-project/init.sh
+++ run-terraform-project/init.sh
@@ -4,18 +4,21 @@
 : "${TF_VAR_product_name?}"
 : "${TF_VAR_component_name?}"
 : "${TF_VAR_region?}"
-: "${CI_S3_MINIO_ADM_ENDPOINT?}"
-: "${CI_MINIO_ADM_SA_ACCESS_KEY?}"
-: "${CI_MINIO_ADM_SA_SECRET_KEY?}"
+: "${MINIO_ADM_S3_ENDPOINT?}"
+: "${MINIO_ADM_SA_ACCESS_KEY?}"
+: "${MINIO_ADM_SA_SECRET_KEY?}"
 
 BACKEND_CONFIG_BUCKET="bucket-${TF_VAR_org_name}-state-${TF_VAR_env}"
+echo "Preparing for Terraform init: bucket=$BACKEND_CONFIG_BUCKET"
 BACKEND_CONFIG_KEY="${TF_VAR_product_name}/${TF_VAR_component_name}/tfstate.json"
+echo "Preparing for Terraform init: key=$BACKEND_CONFIG_KEY"
 BACKEND_CONFIG_REGION="${TF_VAR_region}"
+echo "Preparing for Terraform init: region=$BACKEND_CONFIG_REGION"
 
 terraform init -upgrade \
-  -backend-config=endpoint="${CI_S3_MINIO_ADM_ENDPOINT}" \
-  -backend-config=access_key="${CI_MINIO_ADM_SA_ACCESS_KEY}" \
-  -backend-config=secret_key="${CI_MINIO_ADM_SA_SECRET_KEY}" \
+  -backend-config=endpoint="${MINIO_ADM_S3_ENDPOINT}" \
+  -backend-config=access_key="${MINIO_ADM_SA_ACCESS_KEY}" \
+  -backend-config=secret_key="${MINIO_ADM_SA_SECRET_KEY}" \
   -backend-config=bucket="${BACKEND_CONFIG_BUCKET}" \
   -backend-config=key="${BACKEND_CONFIG_KEY}" \
   -backend-config=region="${BACKEND_CONFIG_REGION}" \
