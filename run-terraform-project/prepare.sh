36eded31eb3dcc1a06f52650e4dd9b8b47d6f9a5
Kristo Aun <kristoaun@live.com>
2024-01-15 10:49:08 +0200

    account auto vars

--- run-terraform-project/prepare.sh
+++ run-terraform-project/prepare.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 : "${TF_VAR_env?}"
 
-ACCOUNT_VARS_FILE=variables/account/${TF_VAR_env}/${TF_VAR_env}.tfvars
+ACCOUNT_VARS_FILE="variables/account.tfvars"
 
 if [ -f ${ACCOUNT_VARS_FILE} ]; then
-  cp ${ACCOUNT_VARS_FILE} ${ENV}.auto.tfvars ;
+  cp ${ACCOUNT_VARS_FILE} account.auto.tfvars ;
 else
   echo "File [${ACCOUNT_VARS_FILE}] not found, skipping."
+fi
+
+ACCOUNT_ENV_VARS_FILE="variables/account/${TF_VAR_env}/${TF_VAR_env}.tfvars"
+
+if [ -f ${ACCOUNT_ENV_VARS_FILE} ]; then
+  cp ${ACCOUNT_ENV_VARS_FILE} ${ENV}.auto.tfvars ;
+else
+  echo "File [${ACCOUNT_ENV_VARS_FILE}] not found, skipping."
 fi
